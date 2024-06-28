c84491b585652ca5c1895b9b309a7e8e8becd15d
GHARAT SIDDHI <111764737+SIDDHI-norahae@users.noreply.github.com>
2024-06-28 13:05:58 +0530

    Add files via upload
    task

---
+++ catshop.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Form validation can be added here
+
+    alert('Thank you for contacting us!');
+});
